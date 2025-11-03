9aa0782384e2b61ee27f0d774963c8c5c5d4a3b8
loowpts <pavelbariik@yandex.ru>
2025-11-03 18:09:08 +0300

    update urls for comments

---
+++ services/content-service/apps/comments/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+app_name = 'comments'
+
+# Все пути через posts/urls.py
+urlpatterns = [
+    
+]
--- services/content-service/apps/posts/urls.py
+++ services/content-service/apps/posts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from apps.interactions import views as interaction_views
+from apps.comments import views as comment_views
 from . import views
 
 app_name = 'posts'
@@ -15,5 +16,11 @@ urlpatterns = [
     path('<slug:post_slug>/like/', interaction_views.post_like_toggle, name='post_like_toggle'),
     path('<slug:post_slug>/likes/', interaction_views.post_likes_list, name='post_likes_list'),
     path('<slug:post_slug>/likes/count/', interaction_views.post_likes_count, name='post_likes_count'),
+    
+    path('<slug:post_slug>/comments/', comment_views.comment_list, name='comment_list'),
+    path('<slug:post_slug>/comments/create/', comment_views.comment_create, name='comment_create'),
+    path('<slug:post_slug>/comments/<int:comment_id>/', comment_views.comment_update, name='comment_update'),
+    path('<slug:post_slug>/comments/<int:comment_id>/delete/', comment_views.comment_delete, name='comment_delete'),
+    path('<slug:post_slug>/comments/<int:comment_id>/reply/', comment_views.comment_reply, name='comment_reply'),
 ]
 
