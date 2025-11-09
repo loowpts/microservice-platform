ee28f9cf1bd468b48080ddff0f477fa5756842f8
loowpts <pavelbariik@yandex.ru>
2025-11-09 18:51:08 +0300

    correcting logic in methods

--- services/marketplace-service/apps/reviews/forms.py
+++ services/marketplace-service/apps/reviews/forms.py
@@ -22,23 +22,21 @@ class ReviewForm(forms.ModelForm):
     def clean_comment(self):
         comment = self.cleaned_data.get('comment')
         
-        if len(comment) > 10:
+        if not comment or len(comment) < 10:
             raise ValidationError('Отзыв должен содержать минимум 10 символов')
         return comment
-    
+
     def clean_pros(self):
         pros = self.cleaned_data.get('pros')
         
-        if pros:
-            if len(pros) < 5:
-                raise ValidationError('Описание "плюсов" должно содержать хотя бы 5 символов.')
+        if pros and len(pros) < 5:
+            raise ValidationError('Описание "плюсов" должно содержать хотя бы 5 символов.')
         return pros
-    
+
     def clean_cons(self):
         cons = self.cleaned_data.get('cons')
         
-        if cons:
-            if len(cons) < 5:
-                raise ValidationError('Описание "минусов" должно содержать хотя бы 5 символов.')
+        if cons and len(cons) < 5:
+            raise ValidationError('Описание "минусов" должно содержать хотя бы 5 символов.')
         return cons
     
