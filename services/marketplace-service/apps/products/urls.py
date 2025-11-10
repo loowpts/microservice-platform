77a0a0984cf3da7a8ea92b7b795031ae885e64f0
loowpts <pavelbariik@yandex.ru>
2025-11-10 14:19:59 +0300

    update urls

--- services/marketplace-service/apps/products/urls.py
+++ services/marketplace-service/apps/products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from apps.favorites import views as favorite_views
+from apps.reviews import views as review_views
 app_name = 'products'
 
 urlpatterns = [
@@ -9,7 +10,12 @@ urlpatterns = [
     path('create/', views.product_create, name='product_create'),
     
     path('<slug:slug>/', views.product_detail, name='product_detail'),
+    
     path('<slug:slug>/favorite/', favorite_views.favorite_toggle, name='favorite_toggle'),
+    
+    path('<slug:slug>/reviews/', review_views.review_list, name='review_list'),
+    path('<slug:slug>/reviews/create/', review_views.review_create, name='review_create'),
+    
     path('<slug:slug>/update/', views.product_update, name='product_update'),
     path('<slug:slug>/delete/', views.product_delete, name='product_delete'),
     
--- services/marketplace-service/config/urls.py
+++ services/marketplace-service/config/urls.py
@@ -3,9 +3,10 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    
     path('api/products', include('apps.products.urls', namespace='products')),
     path('api/categories', include('apps.categories.urls', namespace='categories')),
     path('api/favorites/', include('apps.favorites.urls')),
+    path('api/reviews/', include('apps.reviews', namespace='reviews')),
+    path('api/search/', include('apps.search.urls', namespace='search')),
     
 ]
