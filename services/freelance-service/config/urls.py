89f8e152c46becfeb57b58edd86c65ed60f6bdd2
loowpts <pavelbariik@yandex.ru>
2025-11-23 17:50:55 +0000

    Add Celery configuration and common views to Freelance Service

--- services/freelance-service/config/urls.py
+++ services/freelance-service/config/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from apps.common.views import health_check
 
 urlpatterns = [
+    path('health/', health_check, name='health_check'),
+    
     path("admin/", admin.site.urls),
     
     path('api/gigs/', include('apps.gigs.urls', namespace='gigs')),
